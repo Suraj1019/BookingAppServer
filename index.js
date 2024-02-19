5579259d55fb1d25ac8e455cb1c9b7df54ef4f99
Suraj1019 <surajgautam3110@gmail.com>
2024-02-19 19:59:51 +0530

    created login user api

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ const PORT = process.env.PORT;
 const { connectDB } = require("./mongoConfig");
 const userRoutes = require('./user/userRoutes');
 
-connectDB();
-
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:3000'
 }))
+
 app.use(express.json())
+
+connectDB();
+
 app.use('/user', userRoutes);
 
-console.log("git test")
 app.listen(PORT, () => {
     console.log(`App is listening at port => ${PORT}`)
 })
--- user/userController.js
+++ user/userController.js
@@ -1,13 +1,42 @@
 const user = require('./userModel')
+const bcrypt = require('bcryptjs')
+const jwt = require("jsonwebtoken");
+const { use } = require('./userRoutes');
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
     try {
-        const newUser = await user.create({ name, email, password });
-        res.status(201).json(newUser);
+        const userDoc = await user.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+        });
+        res.status(201).json(userDoc);
+    } catch (error) {
+        res.status(422).json(error);
+    }
+}
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const userDoc = await user.findOne({ email });
+        if (userDoc) {
+            const isPasswordCorrect = bcrypt.compareSync(password, userDoc.password)
+            if (isPasswordCorrect) {
+                jwt.sign({ email: userDoc.email, id: userDoc._id }, process.env.jwtSecret, {}, (err, token) => {
+                    if (err) throw err
+                    res.cookie('token', token).json('loggedIn successfully')
+                })
+            } else {
+                res.status(422).json("incorrect password")
+            }
+        } else {
+            res.json("user does not exist");
+        }
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 }
 
-module.exports = { register };
+module.exports = { register, login };

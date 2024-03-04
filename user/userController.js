71e50294a2861bcc16521a55377f7a4fb7bc6ce8
Suraj1019 <surajgautam3110@gmail.com>
2024-03-05 00:33:18 +0530

    updated login and signup apis

--- user/userController.js
+++ user/userController.js
@@ -1,42 +1,62 @@
-const user = require('./userModel')
-const bcrypt = require('bcryptjs')
+const user = require("./userModel");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { use } = require('./userRoutes');
 
 const register = async (req, res) => {
-    const { name, email, password } = req.body;
-    try {
-        const userDoc = await user.create({
-            name,
-            email,
-            password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+  const { name, email, password } = req.body;
+  try {
+    const userDoc = await user.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcrypt.genSaltSync(10)),
+    });
+    jwt.sign(
+      { email: userDoc.email, id: userDoc._id },
+      process.env.jwtSecret,
+      {},
+      (err, token) => {
+        if (err) throw err;
+        res.json({
+          name: userDoc.name,
+          email: userDoc.email,
+          token: token,
         });
-        res.status(201).json(userDoc);
-    } catch (error) {
-        res.status(422).json(error);
-    }
-}
+      }
+    );
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const login = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const userDoc = await user.findOne({ email });
-        if (userDoc) {
-            const isPasswordCorrect = bcrypt.compareSync(password, userDoc.password)
-            if (isPasswordCorrect) {
-                jwt.sign({ email: userDoc.email, id: userDoc._id }, process.env.jwtSecret, {}, (err, token) => {
-                    if (err) throw err
-                    res.cookie('token', token).json('loggedIn successfully')
-                })
-            } else {
-                res.status(422).json("incorrect password")
-            }
-        } else {
-            res.json("user does not exist");
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+  const { email, password } = req.body;
+  try {
+    const userDoc = await user.findOne({ email });
+    if (userDoc) {
+      const isPasswordCorrect = bcrypt.compareSync(password, userDoc.password);
+      if (isPasswordCorrect) {
+        jwt.sign(
+          { email: userDoc.email, id: userDoc._id },
+          process.env.jwtSecret,
+          {},
+          (err, token) => {
+            if (err) throw err;
+            res.json({
+              name: userDoc.name,
+              email: userDoc.email,
+              token: token,
+            });
+          }
+        );
+      } else {
+        res.status(422).json({ error: "incorrect password" });
+      }
+    } else {
+      res.status(404).json({ error: "user does not exist" });
     }
-}
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 module.exports = { register, login };

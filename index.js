37dfd84a59a8d839c413ffeb7f2fa2d673f328f9
Suraj1019 <surajgautam3110@gmail.com>
2024-07-25 11:47:32 +0530

    added prod-url in cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const placeRoutes = require("./places/placeRoutes");
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://airbnb-booking.netlify.app/"],
   })
 );
 
